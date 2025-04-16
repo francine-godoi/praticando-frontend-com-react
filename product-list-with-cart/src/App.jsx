198711b72735ef249e7af30f6b3bd5146715bb1c
Francine <francine.godoi@gmail.com>
2025-04-16 14:36:43 -0300

    ver mobile + inicio cart

--- product-list-with-cart/src/App.jsx
+++ product-list-with-cart/src/App.jsx
@@ -3,24 +3,53 @@ import ProductCard from "./components/ProductCard";
 
 function App() {
   const [data, setData] = useState([]);
+  const [cartItems, setCartItems] = useState([]);
 
   fetch("../db/data.json")
     .then((response) => response.json())
     .then((data) => setData(data));
 
+  const updateCart = (item) => {
+    setCartItems((c) => c.filter((cartItem) => cartItem.name !== item.name));
+    setCartItems((c) => [...c, item]);
+  };
+
   return (
-    <div className="mx-auto my-15 min-h-dvh w-6xl">
-      <h1 className="mb-8 text-4xl font-bold">Desserts</h1>
-      <div className="grid w-3/4 grid-cols-[repeat(auto-fit,minmax(256px,1fr))] gap-2.5">
-        {data.map((item, index) => (
-          <ProductCard
-            key={index}
-            image={item.image.desktop}
-            name={item.name}
-            category={item.category}
-            price={item.price}
-          />
-        ))}
+    <div
+      id="container"
+      className="mx-5 my-10 flex min-h-dvh flex-col items-center justify-center gap-7 md:flex-row md:items-start"
+    >
+      <div id="products-list-container" className="w-full md:w-3xl">
+        <h1 className="mb-8 text-4xl font-bold">Desserts</h1>
+        <div
+          id="products-grid-container"
+          className="grid grid-cols-1 gap-7 md:grid-cols-[repeat(auto-fit,minmax(200px,1fr))]"
+        >
+          {data.map((item, index) => (
+            <ProductCard
+              key={index}
+              image={item.image}
+              name={item.name}
+              category={item.category}
+              price={item.price}
+              updateCart={updateCart}
+            />
+          ))}
+        </div>
+      </div>
+      <div
+        id="cart-container"
+        className="flex h-fit w-96 flex-col rounded-2xl bg-white p-5"
+      >
+        <h3 className="text-red mb-8 text-xl font-bold">Your Cart (0)</h3>
+        <img
+          className="mx-auto w-32"
+          src="../src/assets/images/illustration-empty-cart.svg"
+          alt=""
+        />
+        <p className="mx-auto my-4 text-sm font-semibold text-rose-500">
+          Your added items will appear here
+        </p>
       </div>
     </div>
   );
--- product-list-with-cart/src/components/ProductCard.jsx
+++ product-list-with-cart/src/components/ProductCard.jsx
@@ -1,19 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 
-const ProductCard = ({ image, name, category, price }) => {
+const ProductCard = ({ image, name, category, price, updateCart }) => {
+  const [openSelector, toggleSelector] = useState(false);
+  const [quantity, setQuantity] = useState(1);
+
+  function openQuantitySelector() {
+    toggleSelector((o) => ({ openSelector: !o.openSelector }));
+    addToCart();
+  }
+
+  function increaseQuantity() {
+    setQuantity((q) => q + 1);
+    addToCart();
+  }
+
+  function decreaseQuantity() {
+    setQuantity((q) => Math.max(q - 1, 1));
+    addToCart();
+  }
+
+  const addToCart = () => {
+    const item = [{ name: name, quantity: quantity, price: price }];
+    updateCart(item);
+  };
+
   return (
-    <div className="mb-5 flex w-fit flex-col gap-10">
-      <div className="relative flex w-fit flex-col items-center">
-        <img className="w-3xs rounded-lg" src={image} alt={name} />
-        <button className="absolute -bottom-1/11 flex w-fit gap-2 rounded-full border border-rose-500 bg-white p-2 pr-7 pl-7 font-semibold text-rose-900">
+    <div id="card-container" className="flex flex-col gap-10">
+      <div
+        id="product-image"
+        className="relative flex w-fit flex-col items-center"
+      >
+        <picture>
+          <source srcSet={image.desktop} media="(min-width: 1024px)" />
+          <source srcSet={image.tablet} media="(min-width: 640px)" />
+          <img
+            className="rounded-lg"
+            src={image.mobile}
+            alt={`${name} image`}
+          />
+        </picture>
+
+        <button
+          onClick={openQuantitySelector}
+          className="hover:text-red hover:border-red absolute -bottom-1/17 flex w-44 gap-2 rounded-full border border-rose-500 bg-white p-2 px-7 font-semibold text-rose-900 hover:cursor-pointer md:-bottom-1/11"
+        >
           <img
             src="../src/assets/images/icon-add-to-cart.svg"
             alt="add to cart icon"
           />
           Add to Cart
         </button>
+        {openSelector && (
+          <div className="bg-red border-red absolute -bottom-1/17 flex w-44 justify-between rounded-full border p-2 px-2.5 font-semibold text-white md:-bottom-1/11">
+            <div
+              onClick={decreaseQuantity}
+              className="hover:text-red flex items-center justify-center rounded-full border border-white px-1.5 hover:cursor-pointer hover:bg-white"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="2"
+                fill="currentColor"
+                viewBox="0 0 10 2"
+              >
+                <path fill="currentColor" d="M0 .375h10v1.25H0V.375Z" />
+              </svg>
+            </div>
+            <p>{quantity}</p>
+            <div
+              onClick={increaseQuantity}
+              className="hover:text-red flex items-center justify-center rounded-full border border-white px-1.5 hover:cursor-pointer hover:bg-white"
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="10"
+                height="10"
+                fill="currentColor"
+                viewBox="0 0 10 10"
+              >
+                <path
+                  fill="currentColor"
+                  d="M10 4.375H5.625V0h-1.25v4.375H0v1.25h4.375V10h1.25V5.625H10v-1.25Z"
+                />
+              </svg>
+            </div>
+          </div>
+        )}
       </div>
-      <div>
+      <div id="product-info">
         <p className="text-sm font-normal text-rose-500">{category}</p>
         <p className="font-semibold text-rose-900">{name}</p>
         <p className="text-red font-semibold">{`$${price.toFixed(2)}`}</p>
